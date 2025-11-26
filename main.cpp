cb0307f4ac53f67951a2e03b3f18238becb4637d
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2025-11-26 20:35:46 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+double calcolaArea(int r1) 
+{
+    double area;
+    area = r1 * r1 * 3.14;
+    return area;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+double calcoloCirconferenza(int r1) 
+{
+    double circonferenza;
+    circonferenza = 6.28 * r1;
+    return circonferenza;
+}
+int main() 
+{
+    int r1, r2;
+    double area, circonferenza;
+    cout << "inserisci il raggio del primo cerchio" << endl;
+    cin >> r1;
+    cout << "inserisci il raggio del secondo cerchio" << endl;
+    cin >> r2;
+    area = calcolaArea(r1);
+    circonferenza = calcoloCirconferenza(r1);
+    cout << "la circonferenza del primo cerchio è " << circonferenza << " e l'area è " << area << endl;
+    area = calcolaArea(r2);
+    circonferenza = calcoloCirconferenza(r2);
+    cout << "la circonferenza del secondo cerchio è " << circonferenza << " e l'area è " << area << endl;
+}
